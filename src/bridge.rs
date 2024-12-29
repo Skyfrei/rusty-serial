6d29d36051ac6df3975e862d983bab04edceffca
Skyfrei <tarkaklavio@gmail.com>
2024-12-29 17:05:44 +0100

    error handling and crash handling

--- src/bridge.rs
+++ src/bridge.rs
@@ -3,7 +3,9 @@ use serialport::SerialPort;
 use serialport::SerialPortInfo;
 use std::io;
 use std::time::Duration;
+use std::u8;
 use crate::args;
+use std::{thread, time};
 
 struct SerialCommunicationSettings{
     comm_speed: u32
@@ -28,9 +30,9 @@ pub fn comm_with_serial_port(){
         let mut connected_port = open_port(&port_list[buffer_to_int].port_name);
         loop {    
             read_serial(&mut connected_port);
-            //write_serial(&mut connected_port);
-
+            write_serial(&mut connected_port);
         }
+        close_port(connected_port);
     }
 }
 
@@ -51,26 +53,53 @@ fn open_port(port_name: &String) -> Box<dyn SerialPort>{
 
     let serial_settings = SerialCommunicationSettings { comm_speed: 115200 };
     let port = serialport::new(port_name, serial_settings.comm_speed)
-        .timeout(Duration::from_millis(10))
+        .timeout(Duration::from_millis(1000))
         .open().expect("Failed to open port");
 
     return port;
 }
 
+//fn read_write_serial(
+//    port: &mut Box<dyn SerialPort>,
+//    read_stats: &mut Stats<'a>, 
+//    write_stats: &mut Stats<'a>
+//){
+//    let mut buf = vec![0u8; read_stats.data.len()];
+//
+//    for _ in 0..read_stats.iterations{
+//        write_stats.start();
+//        
+//
+//    }
+//}
 
 fn read_serial(port: &mut Box<dyn SerialPort>){
-    print!("{}", port.bytes_to_read().expect("Error reading bytes!"));
 
+    let buffer_size: u32 = port.bytes_to_read().expect("Failed to read the port bytes");
+    let mut buffer: Vec<u8> = Vec::new();
+    let res = port.read(&mut buffer);
+    check_for_errors(res);
+
+    print!("There is {} bytes to read.\nBytes: {:?} \n", buffer_size, buffer);
+         
 }
 
-fn write_serial(port: &mut Box<dyn SerialPort>){
+fn check_for_errors(res: Result<usize, io::Error>){
+    match res{
+        Ok(v) => print!("Operation succeessful.\n"),
+        Err(e) => println!("{}\n", e),
+    }
+}
 
+fn write_serial(port: &mut Box<dyn SerialPort>) {
+    println!("\nWrite the serial message!\n");
+    port.flush();
     let mut buffer = String::new();
     let _ = io::stdin().read_line(&mut buffer);
-    port.write(buffer.as_bytes()).expect("Write failed!");
-}
 
-fn write_byte_serial(port: &mut Box<dyn SerialPort>, file_path: &str){
+    let write_buffer = buffer.as_bytes();
+    let res = port.write(&write_buffer);
+    check_for_errors(res);
 
 }
 
